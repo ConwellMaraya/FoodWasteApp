c5bf8b774ceff7e332d5bf8a4d47a5408bd42355
ConwellMaraya <conwell.maraya@gmail.com>
2024-05-05 18:24:26 +0800

    Most of the stuff works, Making a commit cause i need to fix toast notifs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using FoodWasteApp.Windows;
+﻿using CommunityToolkit.WinUI.Notifications;
+using FoodWasteApp.Windows;
 using System.Collections;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Windows;
@@ -13,7 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
-using System.Windows.Shapes; 
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
 
 namespace FoodWasteApp
 {
@@ -26,26 +29,34 @@ namespace FoodWasteApp
         private List<FoodButton> buttonList = new List<FoodButton>();
         
         
-        
+
+
+
         public MainWindow()
         {
-            
-            //TODO
-            //ADD CONSUMED, ROTTEN AND FOOD LIST
-            //INTERFACE
             if (!File.Exists("Pantry_List.json5"))
                 File.Create("Pantry_List.json5").Close();
             if(!File.Exists("Dispose_List.json5"))
                 File.Create("Dispose_List.json5").Close();
             if(!File.Exists("Consumed_List.json5"))
                 File.Create("Consumed_List.json5").Close();
+            if (File.Exists("Settings.ini"))
+                File.Create("Settings.ini").Close();
 
 
             InitializeComponent();
             Application.Current.MainWindow.Closing += new CancelEventHandler(onWindowClose);
             setupPantryGrid("Pantry_List.Json5");
             setLabelDate();
-            ClearGrid.Click += new RoutedEventHandler(Clear_Click);
+            // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("Andrew sent you a picture")
+                .AddText("Check this out, The Enchantments in Washington!");
+                 // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
+
+
 
 
 
@@ -61,6 +72,12 @@ namespace FoodWasteApp
             Date_Tracker.Content = "Today is: " + DateTime.Now.ToString("MM/dd/yyyy");
         }
 
+        /// <summary>
+        /// <para>Input: String fileName </para>
+        /// <para>Output: Void</para>
+        /// <para>Process: Sets up the grid to represent your pantry during application startup. uses the fileName to load up the Pantry_List file</para>
+        /// </summary>
+        /// <param name="fileName"></param>
         private void setupPantryGrid(string fileName)
         {
 
@@ -127,13 +144,37 @@ namespace FoodWasteApp
             using(StreamReader file = new StreamReader(fileName))
             {
                 string ln = "";
+                Dictionary<string, int> expiredFood = new Dictionary<string, int>();
                 while ((ln = file.ReadLine()) != null)
                 {
                     string foodName = ln.Substring(0, ln.IndexOf(':'));
                     DateTime expDate;
                     if (DateTime.TryParse(ln.Substring(ln.IndexOf(":") + 1), out expDate)) 
                     {
-                        AddItemToGrid(foodName, expDate.ToString("MM/dd/yyyy"));
+                        if (expDate >= DateTime.Today.Date)
+                        {
+                            AddItemToGrid(foodName, expDate.ToString("MM/dd/yyyy"));
+                        }
+                        else 
+                        {
+                            addToList("Dispose_List.json5", foodName);
+                            if (expiredFood.ContainsKey(foodName))
+                                expiredFood[foodName]++;
+                            else
+                                expiredFood.Add(foodName, 1);
+                        }
+                    }
+
+                    if (expiredFood.Count > 0) 
+                    {
+                        string msg = "These foods have expired, please dispose of them immediately:\n";
+                        foreach(string x in expiredFood.Keys)
+                        { 
+                            msg += (x + ":" + expiredFood[x] + '\n');
+                        }
+
+                        System.Windows.MessageBox.Show(msg);
+                    
                     }
 
 
@@ -145,7 +186,11 @@ namespace FoodWasteApp
             }
 
         }
-
+        /// <summary>
+        /// <para>Input: Void</para>
+        /// <para>Output: Void</para>
+        /// <para>Process: Sets up the grid to represent your pantry during Grid Reload</para>
+        /// </summary>
         private void setupPantryGrid()
         {
 
@@ -211,6 +256,13 @@ namespace FoodWasteApp
 
         }
 
+        /// <summary>
+        /// <para>Input: object Sender RoutedEventsArgs e</para>
+        /// <para>Output: Void</para>
+        /// <para>Process: Brings up the <see cref="AddItemPopup.AddItemPopup"/>, and when window closes uses logic to determine if an item will be added to the PantryGrid</para>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddItem_Click(object sender, RoutedEventArgs e)
         {
             AddItemPopup a = new AddItemPopup();
@@ -236,22 +288,30 @@ namespace FoodWasteApp
 
         }
 
-        private void AddItemToGrid(string s, string t)
+        /// <summary>
+        /// <para>Input: String foodName String expriyDate</para>
+        /// <para>Output: Void</para>
+        /// <para>Process: Adds an Item into the PantryGrid, Creates a <see cref="FoodButton"/> and a Textblock and puts them into the Grid</para>
+        /// </summary>
+        /// <param name="foodName"></param>
+        /// <param name="expiryDate"></param>
+        private void AddItemToGrid(string foodName, string expiryDate)
         {
             FoodButton x = new FoodButton
             {
-                Content = s,
+                Content = foodName,
                 HorizontalContentAlignment = HorizontalAlignment.Center,
+                VerticalContentAlignment = VerticalAlignment.Top,
                 Width = 300,
                 Height = 30,
                 Margin = new Thickness(0, 0, 0, 0),
                 Foreground = new SolidColorBrush(Colors.White),
                 Background = new SolidColorBrush(Colors.Black),
                 HorizontalAlignment = HorizontalAlignment.Stretch,
-                VerticalAlignment = VerticalAlignment.Top,
+                VerticalAlignment = VerticalAlignment.Center,
                 FontFamily = new FontFamily("Kayak Sans"),
                 FontSize = 15,
-                ExpDate = t,
+                ExpDate = expiryDate,
                 BorderThickness = new Thickness(0)
 
 
@@ -260,14 +320,13 @@ namespace FoodWasteApp
             x.Click += new RoutedEventHandler(FoodButtonClicked);
             Pantry_Grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(30) });
             Pantry_Grid.Children.Add(x);
-            x.rowIndex = Pantry_Grid.RowDefinitions.Count-1;
             buttonList.Add(x);
             Grid.SetRow(x, Pantry_Grid.RowDefinitions.Count - 1);
             Grid.SetColumn(x, 0);
 
             TextBlock y = new TextBlock
             {
-                Text = t,
+                Text = expiryDate,
                 TextAlignment = TextAlignment.Center,
                 Width = 300,
                 Height = 30,
@@ -275,7 +334,7 @@ namespace FoodWasteApp
                 Foreground = new SolidColorBrush(Colors.White),
                 Background = new SolidColorBrush(Colors.Black),
                 HorizontalAlignment = HorizontalAlignment.Stretch,
-                VerticalAlignment = VerticalAlignment.Top,
+                VerticalAlignment = VerticalAlignment.Center,
                 FontFamily = new FontFamily("Kayak Sans"),
                 FontSize = 15,
 
@@ -290,11 +349,18 @@ namespace FoodWasteApp
             return;
         }
 
-        private void RebuildGridHelper(string s, string t)
+        /// <summary>
+        /// <para>Input: String foodName String expriyDate</para>
+        /// <para>Output: Void</para>
+        /// <para>Process: Same as <see cref="AddItemToGrid(string, string)"/>, but removed the line to add items to <see cref="buttonList"/> to avoid modifying the list</para>
+        /// </summary>
+        /// <param name="foodName"></param>
+        /// <param name="expiryDate"></param>
+        private void RebuildGridHelper(string foodName, string expDate)
         {
             FoodButton x = new FoodButton
             {
-                Content = s,
+                Content = foodName,
                 HorizontalContentAlignment = HorizontalAlignment.Center,
                 Width = 300,
                 Height = 30,
@@ -305,7 +371,7 @@ namespace FoodWasteApp
                 VerticalAlignment = VerticalAlignment.Top,
                 FontFamily = new FontFamily("Kayak Sans"),
                 FontSize = 15,
-                ExpDate = t,
+                ExpDate = expDate,
                 BorderThickness = new Thickness(0)
 
 
@@ -314,13 +380,12 @@ namespace FoodWasteApp
             x.Click += new RoutedEventHandler(FoodButtonClicked);
             Pantry_Grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(30) });
             Pantry_Grid.Children.Add(x);
-            x.rowIndex = Pantry_Grid.RowDefinitions.Count - 1;
             Grid.SetRow(x, Pantry_Grid.RowDefinitions.Count - 1);
             Grid.SetColumn(x, 0);
 
             TextBlock y = new TextBlock
             {
-                Text = t,
+                Text = expDate,
                 TextAlignment = TextAlignment.Center,
                 Width = 300,
                 Height = 30,
@@ -341,7 +406,15 @@ namespace FoodWasteApp
             return;
         }
 
-        public void RebuildAfterDelete()
+        
+
+        /// <summary>
+        /// <para>Input:Void</para>
+        /// <para>Output:Void</para>
+        /// <para>Process:Called when an item is deleted from PantryGrid, remakes the Grid to where the item does not exist in it</para>
+        /// </summary>
+        
+        public void RebuildGrid()
         {
             Pantry_Grid.Children.Clear();
             Pantry_Grid.RowDefinitions.Clear();
@@ -353,32 +426,34 @@ namespace FoodWasteApp
 
             foreach (FoodButton b in buttonList)
             {
+                //Will never be null despite the warning
                 RebuildGridHelper(b.Content.ToString(), b.ExpDate);
             }
 
             buttonList.Clear();
 
-            foreach(var x in  Pantry_Grid.Children)
-            {
-                if (x.GetType() == typeof(FoodButton))
-                {
-                    FoodButton temp = (FoodButton)x;
-                    buttonList.Add(temp);
-                }
-            }
+            buttonList = Pantry_Grid.Children.OfType<FoodButton>().ToList();
+
+
+
+            
+
 
             
             
         }
+
+
+        /// <summary>
+        /// <para>Input: object sender, RouterEventArgs 3</para>
+        /// <para>Output: Void</para>
+        /// <para>Process: Handles the event for when an item in the grid is clicked, Shows a window containing the Food Item's Name, Expiration Date and the options for the item.</para>
+        /// </summary>
         public void FoodButtonClicked(object sender, RoutedEventArgs e)
         {
             FoodButton b = (FoodButton)sender;
             
-            int x = Pantry_Grid.Children.IndexOf(b);
-            TextBlock t = ((TextBlock)Pantry_Grid.Children[x+1]);
-            string Datestring = ((TextBlock)Pantry_Grid.Children[x+1]).Text;
-            string FoodName = ((FoodButton)Pantry_Grid.Children[x]).Content.ToString();
-            ItemWindowPopup i = new ItemWindowPopup(FoodName,Datestring);   
+            ItemWindowPopup i = new ItemWindowPopup(b.Content.ToString(),b.ExpDate);   
             i.ShowDialog();
             int retcode = i.retInt;
 
@@ -387,19 +462,12 @@ namespace FoodWasteApp
                 case 0:
                     addToList("Consumed_List.json5", b.Content.ToString());
                     buttonList.Remove(b);
-                    var watch = System.Diagnostics.Stopwatch.StartNew();
-                    RebuildAfterDelete();
-                    watch.Stop();
-                    var elapsedMs = watch.ElapsedMilliseconds;
-                    MessageBox.Show(elapsedMs.ToString());
-                    
-                    
-                    
-                    
+                    RebuildGrid();
                     break;
                 case 1:
+                    addToList("Disposed_List.json5", b.Content.ToString());
                     buttonList.Remove(b);
-                    RebuildAfterDelete();
+                    RebuildGrid();
                     break;
                 default:
                     break;
@@ -425,6 +493,13 @@ namespace FoodWasteApp
             setupPantryGrid();
         }
 
+
+        /// <summary>
+        /// <para>Input:String fileName String foodName</para>
+        /// <para>Output: Void</para>
+        /// <para>Process:Given a fileName and a foodName, Check if the foodName exists in a file.</para>
+        /// <para>If it does not, Append the food to the end of the file, if it does then increment the value next to the foodName by one.</para>
+        /// </summary>
         public void addToList(string fileName, string foodName)
         {
             string ln;
@@ -468,36 +543,7 @@ namespace FoodWasteApp
             }
         }
 
-        static void addToFoodList(string fileName, string foodName)
-        {
-            bool lineFlag = false;
-            string ln = "";
-            using (StreamReader file = new StreamReader(fileName))
-            {
-                while ((ln = file.ReadLine()) != null)
-                {
-                    string check = ln.Substring(0, ln.IndexOf(':'));
-
-                    if (check == foodName)
-                    {
-                        lineFlag = true;
-                        break;
-                    }
-                }
-            }
-
-            if (lineFlag)
-            {
-                return;
-            }
-            else
-            {
-                using (TextWriter t = new StreamWriter(fileName, true))
-                { 
-                    t.WriteLine(foodName);
-                }
-            }
-        }
+        
 
         static void lineChanger(string newText, string fileName, int line_to_edit)
         {
@@ -506,33 +552,51 @@ namespace FoodWasteApp
             File.WriteAllLines(fileName, arrLine);
         }
 
+        static void settingReader(string fileName, int lineNum)
+        {
+            string[] arrLine = File.ReadAllLines(fileName);
+
+        }
+
+
+        /// <summary>
+        /// <para>Input:object sender EventArgs e</para>
+        /// <para>Output: Void</para>
+        /// <para>Process: Saves all food items loaded into memory into Pantry_List.json5</para>
+        /// </summary>
+        
+        //TODO: Backups
         private void onWindowClose(object sender, EventArgs e) 
         {
+            
             File.Create("Pantry_List.json5").Close();
             
 
-            foreach (var x in Pantry_Grid.Children)
+            foreach (FoodButton x in buttonList)
             {
-                if (x.GetType() == typeof(FoodButton))
-                {
-                    FoodButton btn = (FoodButton)x;
                     using (TextWriter txt = new StreamWriter("Pantry_List.json5", true))
                     {
-                        txt.WriteLine(btn.Content.ToString() + ":" + btn.ExpDate);
+                        txt.WriteLine(x.Content.ToString() + ":" + x.ExpDate);
                         txt.Close();
                     }
                     
-                }
             }
 
+            Environment.Exit(0);
+
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settingsWindow = new SettingsWindow();
+            settingsWindow.Show();
         }
     }
 
-    
+
 
     public class FoodButton : Button
     {
-        public int rowIndex { get; set; }
         public string ExpDate { get; set; }
     }
 
---
+++ Windows/SettingsWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+
+namespace FoodWasteApp.Windows
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        
+        public SettingsWindow()
+        {
+            
+            InitializeComponent();
+            PantryBGColor.Visibility = Visibility.Hidden;
+            PantryTextColor.Visibility = Visibility.Hidden;
+            PantryBGColor.SelectedColorChanged += PantryBGColor_SelectedColorChanged;
+            PantryBGColor.SelectedColor = (Color)ColorConverter.ConvertFromString("#FF000000");
+
+
+
+
+
+
+        }
+
+        private void PantryBGColor_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            ColorPicker s = (ColorPicker)sender;
+            System.Windows.MessageBox.Show(s.SelectedColor.ToString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PantryTextColor.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            PantryBGColor.Visibility = Visibility.Visible;
+        }
+
+
+    }
+}
